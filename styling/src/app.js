089709c59323ea14ce820f6c524d4b9dbbe5b640
Mustafa Can <mustafaa.can@gmail.com>
2026-04-13 00:20:00 +0300

    STYLING

---
+++ styling/src/app.js
@@ -0,0 +1,39 @@
+// INLINE : style={{...}} --> color:"red" (key-value pair)
+
+export default function App() {
+  return (
+    <div style={{ color: "red", backgroundColor: "whitesmoke" }}>
+      <p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nihil odio
+          reprehenderit quis incidunt aliquid iste nesciunt debitis autem velit,
+          tempore officiis fuga, dicta sint esse deleniti, molestiae sed ab.
+        </span>
+        <span>
+          Laudantium delectus omnis, culpa fugit ab illo esse praesentium
+          obcaecati, ea pariatur aspernatur? Labore tempora quia cupiditate,
+          nisi iure voluptatem excepturi pariatur, possimus illum doloribus
+          nesciunt vitae, asperiores perferendis. Non.
+        </span>
+        <span>
+          Distinctio reiciendis, pariatur earum itaque iure odit tempore ipsum,
+          laborum repellendus dolorum modi corrupti consectetur dolor aliquam
+          laboriosam debitis fuga aperiam impedit at. Illum quod quisquam quam
+          magnam necessitatibus dignissimos!
+        </span>
+        <span>
+          Ea magni totam recusandae assumenda consectetur, reiciendis iste
+          aliquid eos quam error, dolorum suscipit pariatur laborum modi
+          similique impedit quae sequi vel numquam id ipsam provident sint et?
+          Architecto, adipisci?
+        </span>
+        <span>
+          Architecto error aliquid repudiandae repellat suscipit recusandae
+          doloremque fugit molestias quisquam dolorum, commodi magni laboriosam
+          quaerat, animi autem accusamus iure! Tenetur nisi hic ipsam! Veniam
+          molestias perspiciatis officiis sequi maiores?
+        </span>
+      </p>
+    </div>
+  );
+}
