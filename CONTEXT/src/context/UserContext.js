a556406603e02206e3f67039209d6f60da16d9fd
Mustafa Can <mustafaa.can@gmail.com>
2026-05-07 03:22:00 +0300

    CONTEXT

--- CONTEXT/src/context/UserContext.js
+++ CONTEXT/src/context/UserContext.js
@@ -1,11 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [login, setLogin] = useState(null);
+  const readFromStorage = (() => {
+    try {
+      const storedUser = localStorage.getItem("loggedIn");
+      return storedUser ? JSON.parse(storedUser) : null;
+    } catch (error) {
+      localStorage.removeItem("loggedIn");
+      return null;
+    }
+  })();
+
+  const [login, setLogin] = useState(readFromStorage);
 
   const values = { login, setLogin };
+
+  useEffect(() => {
+    if (login) {
+      localStorage.setItem("loggedIn", JSON.stringify(login));
+    } else {
+      localStorage.removeItem("loggedIn");
+    }
+  }, [login]);
 
   return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
 };
